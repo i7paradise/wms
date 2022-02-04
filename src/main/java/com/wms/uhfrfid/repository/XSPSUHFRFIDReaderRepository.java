dfd12a4cd2d375d9df7a50f10850a2b9039186f4
Ismail Ferdous <ismail.ferdous@gmail.com>
2022-02-05 00:30:11 +0100

    bi directional : UHFRFIDAntenna to UHFRFIDReader

--- src/main/java/com/wms/uhfrfid/repository/XSPSUHFRFIDReaderRepository.java
+++ src/main/java/com/wms/uhfrfid/repository/XSPSUHFRFIDReaderRepository.java
@@ -1,6 +1,8 @@
 package com.wms.uhfrfid.repository;
 
 import com.wms.uhfrfid.domain.UHFRFIDReader;
+
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Repository;
  * Spring Data SQL repository for the DeliveryOrder entity.
  */
 @Repository
+@Primary
 public interface XSPSUHFRFIDReaderRepository extends UHFRFIDReaderRepository {
     //TODO define Query
     Page<UHFRFIDReader> findUHFReaderByName(String name, Pageable pageable);

ffb243a2fc25bffed047f2fe7b9d273e9088bc6e
Ismail Ferdous <ismail.ferdous@gmail.com>
2022-02-05 01:20:17 +0100

    wip 3

--- src/main/java/com/wms/uhfrfid/service/dto/UHFRFIDReaderDTOV2.java
+++ src/main/java/com/wms/uhfrfid/service/dto/UHFRFIDReaderDTOV2.java
@@ -1,6 +1,7 @@
 package com.wms.uhfrfid.service.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
@@ -8,11 +9,11 @@ public class UHFRFIDReaderDTOV2 extends UHFRFIDReaderDTO {
 
     private List<UHFRFIDAntennaDTO> uhfRFIDAntennas;
 
-    public void setUHFRFIDAntennas(List<UHFRFIDAntennaDTO> uhfRFIDAntennas) {
-        this.uhfRFIDAntennas = uhfRFIDAntennas;
+    public List<UHFRFIDAntennaDTO> getUhfRFIDAntennas() {
+        return uhfRFIDAntennas;
     }
 
-    public List<UHFRFIDAntennaDTO> getDeliveryOrderItems() {
-        return uhfRFIDAntennas;
+    public void setUhfRFIDAntennas(List<UHFRFIDAntennaDTO> uhfRFIDAntennas) {
+        this.uhfRFIDAntennas = uhfRFIDAntennas;
     }
 }

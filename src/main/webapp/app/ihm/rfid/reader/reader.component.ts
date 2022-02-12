25d1c6a8af3fc09c8001b56f7d17b615de4da304
i7paradise <ismailferdous@gmail.com>
2022-02-13 00:11:45 +0100

    extends UHFRFIDReaderComponent + copy/paste uhfrfid-reader.component.html into reader.component.html

--- src/main/webapp/app/ihm/rfid/reader/reader.component.ts
+++ src/main/webapp/app/ihm/rfid/reader/reader.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { UHFRFIDReaderComponent } from 'app/entities/uhfrfid-reader/list/uhfrfid-reader.component';
 
 @Component({
   selector: 'jhi-reader',
   templateUrl: './reader.component.html',
   styleUrls: ['./reader.component.scss']
 })
-export class ReaderComponent implements OnInit {
+export class ReaderComponent extends UHFRFIDReaderComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+ 
 
 }

c8a507e02a4949c2080e07e83c44ae8ef678e49c
i7paradise <ismailferdous@gmail.com>
2022-02-13 01:24:07 +0100

    impl ReaderResource

--- src/main/java/com/wms/uhfrfid/web/rest/ReaderResource.java
+++ src/main/java/com/wms/uhfrfid/web/rest/ReaderResource.java
@@ -1,25 +1,39 @@
 package com.wms.uhfrfid.web.rest;
 
+import com.wms.uhfrfid.service.ReaderService;
+import com.wms.uhfrfid.service.dto.UHFRFIDReaderDTOV2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.jhipster.web.util.ResponseUtil;
+
+import java.util.Optional;
 
 /**
  * ReaderResource controller
  */
 @RestController
-@RequestMapping("/api/reader")
+@RequestMapping(ReaderResource.PATH)
 public class ReaderResource {
 
-    private final Logger log = LoggerFactory.getLogger(ReaderResource.class);
+    public static final String PATH = "/api/v1/reader";
 
-    /**
-     * GET getReader
-     */
-    @GetMapping("/get-reader")
-    public String getReader() {
-        return "getReader";
+    private final Logger log = LoggerFactory.getLogger(ReaderResource.class);
+    private final ReaderService readerService;
+
+    public ReaderResource(ReaderService readerService) {
+        this.readerService = readerService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UHFRFIDReaderDTOV2> getReader(@PathVariable Long id) {
+        log.debug("REST request to get Order : {}", id);
+        Optional<UHFRFIDReaderDTOV2> readerDTO = readerService.findOneDetailed(id);
+        return ResponseUtil.wrapOrNotFound(readerDTO);
+
     }
 }
--- src/test/java/com/wms/uhfrfid/web/rest/ReaderResourceIT.java
+++ src/test/java/com/wms/uhfrfid/web/rest/ReaderResourceIT.java
@@ -1,14 +1,6 @@
 package com.wms.uhfrfid.web.rest;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.wms.uhfrfid.IntegrationTest;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 /**
  * Test class for the ReaderResource REST controller.
@@ -18,21 +10,5 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 @IntegrationTest
 class ReaderResourceIT {
 
-    private MockMvc restMockMvc;
-
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-
-        ReaderResource readerResource = new ReaderResource();
-        restMockMvc = MockMvcBuilders.standaloneSetup(readerResource).build();
-    }
-
-    /**
-     * Test getReader
-     */
-    @Test
-    void testGetReader() throws Exception {
-        restMockMvc.perform(get("/api/reader/get-reader")).andExpect(status().isOk());
-    }
+   //TODO
 }

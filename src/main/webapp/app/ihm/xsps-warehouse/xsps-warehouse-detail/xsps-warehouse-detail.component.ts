2be12d294d0775377eb6996aedb385be1e4c134d
efabor <efabor@gmail.com>
2022-02-18 18:05:19 +0100

    copy/paste warehouse-detail.component.html -> xsps-warehouse-detail.component.html

--- src/main/webapp/app/ihm/xsps-warehouse/xsps-warehouse-detail/xsps-warehouse-detail.component.ts
+++ src/main/webapp/app/ihm/xsps-warehouse/xsps-warehouse-detail/xsps-warehouse-detail.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { WarehouseDetailComponent } from 'app/entities/warehouse/detail/warehouse-detail.component';
 
 @Component({
   selector: 'jhi-xsps-warehouse-detail',
   templateUrl: './xsps-warehouse-detail.component.html',
   styleUrls: ['./xsps-warehouse-detail.component.scss']
 })
-export class XspsWarehouseDetailComponent implements OnInit {
+export class XspsWarehouseDetailComponent extends WarehouseDetailComponent {
 
-  constructor() { }
+  constructor(  protected activatedRoute: ActivatedRoute) {
+      super(activatedRoute);
+    }
 
   ngOnInit(): void {
+      super.ngOnInit();
   }
 
 }
